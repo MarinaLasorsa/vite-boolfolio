cb8bd7f108818d66ae8688d80152a87b91045fbf
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-06-12 16:25:14 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+
+import AppMain from './components/AppMain.vue'
+
+export default {
+
+  components: {
+    AppMain
+  },
+
+  data() {
+    return {
+      message: 'Hello World'
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use './style/general';
+</style>
