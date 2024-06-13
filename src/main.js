d712adf3dba0ad30b1e516270234811d7918ae7e
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-06-13 15:25:12 +0200

    ADD: home, portfolio and contact routes through createRouter + empty AppHome, AppPortfolio and AppContact pages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 //import './style.css'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', name: 'home', component: AppHome },
+        { path: '/portfolio', name: 'portfolio', component: AppPortfolio },
+        { path: '/contact', name: 'contact', component: AppContact }
+    ]
+})
+
+createApp(App).use(router).mount('#app')
